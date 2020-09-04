50754a633e4fa621ea98c2edf1d99d6979b2aa3d
Sam Zhou <sam@developersam.com>
2020-09-03 22:29:17 -0400

    [repo] Enforce that we use type import whenever possible (#41)

--- src/dependency-analysis/dependency-graph-builder.ts
+++ src/dependency-analysis/dependency-graph-builder.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import { Project, SourceFile } from 'ts-morph';
-import { Graph } from './dependency-graph-types';
+import type { Graph } from './dependency-graph-types';
 
 const getImports = (projectDirectory: string, sourceFile: SourceFile): readonly string[] => {
   const imports: string[] = [];
