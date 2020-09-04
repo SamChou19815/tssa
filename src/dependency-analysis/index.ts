0d3c979aa001cdfb36bff67ead144486cc1ae484
Sam <sam@developersam.com>
2020-09-03 22:33:07 -0400

    [analysis] Output dependency chain

--- src/dependency-analysis/index.ts
+++ src/dependency-analysis/index.ts
@@ -1,8 +1,21 @@
+import dependencyChainBuilder from './dependency-graph-analyzer';
 import buildDependencyGraph from './dependency-graph-builder';
 import simplifyDependencyGraphWithDroppedExtensions from './dependency-graph-simplifier';
 import type { Graph } from './dependency-graph-types';
 
-const performDependencyAnalysisForProject = (projectDirectory: string): Graph =>
-  simplifyDependencyGraphWithDroppedExtensions(buildDependencyGraph(projectDirectory));
+type DependencyAnalysisResult = {
+  readonly graph: Graph;
+  readonly dependencyChain: readonly string[];
+};
+
+const performDependencyAnalysisForProject = (
+  projectDirectory: string
+): DependencyAnalysisResult => {
+  const rawGraph = buildDependencyGraph(projectDirectory);
+  return {
+    graph: simplifyDependencyGraphWithDroppedExtensions(rawGraph),
+    dependencyChain: dependencyChainBuilder(rawGraph),
+  };
+};
 
 export default performDependencyAnalysisForProject;
