6835157438e8493eed8c9a76723004e7caecf7b6
Sam <sam@developersam.com>
2020-09-04 22:05:15 -0400

    [cli] Partition projects and changed paths

---
+++ src/changed-modules-partition.ts
@@ -0,0 +1,30 @@
+import { relative } from 'path';
+
+type ProjectAndChangedPaths = {
+  readonly projectPath: string;
+  readonly changedModulePaths: readonly string[];
+};
+
+/** Given a project path, partition changed files into with relativized module paths scoped with projects. */
+const partitionProjectChangedModulePaths = (
+  projectPathsRelativeToRepositoryRoot: readonly string[],
+  allChangedModulePaths: readonly string[]
+): readonly ProjectAndChangedPaths[] => {
+  const map = new Map<string, string[]>();
+  allChangedModulePaths.forEach((changedModulePath) => {
+    const projectRootPath = projectPathsRelativeToRepositoryRoot.find((projectPath) =>
+      changedModulePath.startsWith(projectPath)
+    );
+    if (projectRootPath != null) {
+      const paths = map.get(projectRootPath) || [];
+      paths.push(relative(projectRootPath, changedModulePath));
+      map.set(projectRootPath, paths);
+    }
+  });
+  return Array.from(map.entries()).map(([projectPath, changedModulePaths]) => ({
+    projectPath,
+    changedModulePaths,
+  }));
+};
+
+export default partitionProjectChangedModulePaths;
