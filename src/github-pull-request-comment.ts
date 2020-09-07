0ed03ab2ae820c183431a3491aa1b26ed58e0665
Sam Zhou <sam@developersam.com>
2020-09-07 18:00:12 -0400

    [cli] Add comment to PR in CI environment (#56)

--- src/github-pull-request-comment.ts
+++ src/github-pull-request-comment.ts
@@ -1,23 +1,18 @@
 import { Octokit } from '@octokit/rest';
 
-const getOctokit = (githubToken: string): Octokit =>
+const getOctokit = (): Octokit =>
   new Octokit({
-    auth: `token ${githubToken}`,
-    userAgent: 'cornell-dti/big-diff-warning',
+    auth: `token ${process.env.GITHUB_TOKEN}`,
+    userAgent: 'developer-sam',
   });
 
-const commentOnPullRequest = async (
-  userLogin: string,
-  githubToken: string,
-  prefix: string,
-  comment: string
-): Promise<void> => {
+const commentOnPullRequest = async (prefix: string, comment: string): Promise<void> => {
   const ownerAndRepository = process.env.GITHUB_REPOSITORY?.split('/');
   const githubPullRequestNumber = process.env.GITHUB_PR_NUMBER;
   if (ownerAndRepository == null || githubPullRequestNumber == null) throw new Error();
   const [owner, repo] = ownerAndRepository;
   const number = parseInt(githubPullRequestNumber, 10);
-  const octokit = getOctokit(githubToken);
+  const octokit = getOctokit();
   const { data: comments } = await octokit.issues.listComments({
     owner,
     repo,
@@ -25,9 +20,10 @@ const commentOnPullRequest = async (
   });
   const existingCommentFromThisPackage = comments.find(
     (existingComment) =>
-      existingComment.user.login === userLogin && existingComment.body.startsWith(prefix)
+      existingComment.user.login === process.env.USER_LOGIN &&
+      existingComment.body.startsWith(prefix)
   );
-  const body = `${prefix} ${comment}`;
+  const body = `${prefix}${comment}`;
   if (existingCommentFromThisPackage == null) {
     await octokit.issues.createComment({
       owner,
