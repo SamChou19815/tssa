30d3eff4a9145aabdd84a9f0e27f7cdfdacdd2bf
Sam <sam@developersam.com>
2020-10-03 00:19:17 -0400

    [github] Fetch diff using github API in CI

--- src/github-operations.ts
+++ src/github-operations.ts
@@ -6,12 +6,37 @@ const getOctokit = (): Octokit =>
     userAgent: 'developer-sam',
   });
 
-const commentOnPullRequest = async (prefix: string, comment: string): Promise<void> => {
+type PullRequest = {
+  readonly owner: string;
+  readonly repo: string;
+  readonly number: number;
+};
+
+const getPullRequest = (): PullRequest => {
   const ownerAndRepository = process.env.GITHUB_REPOSITORY?.split('/');
   const githubPullRequestNumber = process.env.GITHUB_PR_NUMBER;
   if (ownerAndRepository == null || githubPullRequestNumber == null) throw new Error();
   const [owner, repo] = ownerAndRepository;
   const number = parseInt(githubPullRequestNumber, 10);
+
+  return { owner, repo, number };
+};
+
+export const getPullRequestDiff = async (): Promise<string> => {
+  const { owner, repo, number } = getPullRequest();
+  const octokit = getOctokit();
+  const { data: diff } = await octokit.pulls.get({
+    owner,
+    repo,
+    pull_number: number,
+    headers: { accept: 'application/vnd.github.v3.diff' },
+  });
+  // The type definition cannot understand using `vnd.github.v3.diff` will return a diff string.
+  return (diff as unknown) as string;
+};
+
+export const commentOnPullRequest = async (prefix: string, comment: string): Promise<void> => {
+  const { owner, repo, number } = getPullRequest();
   const octokit = getOctokit();
   const { data: comments } = await octokit.issues.listComments({
     owner,
@@ -38,5 +63,3 @@ const commentOnPullRequest = async (prefix: string, comment: string): Promise<vo
     body,
   });
 };
-
-export default commentOnPullRequest;
