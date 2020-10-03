990f5f06e4127ec7d784b4fe9bba8954e1f52d73
Sam <sam@developersam.com>
2020-10-02 20:36:12 -0400

    [analysis] Add unit test for partitionProjectChangedModulePaths

---
+++ src/__test__/changed-modules-partition.test.ts
@@ -0,0 +1,28 @@
+import partitionProjectChangedModulePaths from '../changed-modules-partition';
+
+it('partitionProjectChangedModulePaths works on single root.', () => {
+  expect(partitionProjectChangedModulePaths(['.'], ['src/foo.ts', 'src/bar.ts'])).toEqual([
+    {
+      projectPath: '.',
+      changedModulePaths: ['src/foo.ts', 'src/bar.ts'],
+    },
+  ]);
+});
+
+it('partitionProjectChangedModulePaths works on multi-workspaces.', () => {
+  expect(
+    partitionProjectChangedModulePaths(
+      ['packages/foo', 'packages/bar'],
+      ['packages/foo/foo.ts', 'packages/bar/bar.ts']
+    )
+  ).toEqual([
+    {
+      projectPath: 'packages/foo',
+      changedModulePaths: ['foo.ts'],
+    },
+    {
+      projectPath: 'packages/bar',
+      changedModulePaths: ['bar.ts'],
+    },
+  ]);
+});
