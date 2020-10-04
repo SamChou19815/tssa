eda69d0a43086db491c43ee3387ab6d1d26464a0
Sam Zhou <sam@developersam.com>
2020-10-03 20:28:28 -0400

    [misc] Apply fixes to make tssa work for yarn v2 monorepo (#95)
    - Do not use fs.readSync to read stdin to avoid conflicting with yarn v2's patch on fs.
    - Allow missing typescript project config.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
 #!/usr/bin/env node
 
-import { readFileSync } from 'fs';
-
 import { getPullRequestDiff, commentOnPullRequest } from './github-operations';
 import getTSSAResult from './main-analyzer';
 import { tssaResultToString } from './tssa-result';
 
+const getStandardIn = async (): Promise<string> =>
+  new Promise((resolve) => {
+    let content = '';
+    process.stdin.resume();
+    process.stdin.on('data', (buffer) => {
+      content += buffer.toString();
+    });
+    process.stdin.on('end', () => resolve(content));
+  });
+
 const main = async () => {
   const onCI = Boolean(process.env.CI);
   const analysisResultString = tssaResultToString(
-    getTSSAResult(
-      process.argv.slice(2),
-      onCI ? await getPullRequestDiff() : readFileSync(process.stdin.fd).toString()
-    )
+    getTSSAResult(process.argv.slice(2), onCI ? await getPullRequestDiff() : await getStandardIn())
   );
 
   // eslint-disable-next-line no-console
