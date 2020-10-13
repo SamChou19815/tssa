17da12c6e5b879cf480d40a013a44129644fd9f4
Sam <sam@developersam.com>
2020-10-13 16:27:18 -0400

    [analysis] Drop support for css module analysis
    It's too noisy :(

--- src/main-analyzer.ts
+++ src/main-analyzer.ts
@@ -2,12 +2,6 @@
 
 import { normalize } from 'path';
 
-import partitionProjectChangedModulePaths from './changed-modules-partition';
-import { getTopologicallyOrderedTransitiveDependencyChainFromTSModules } from './dependency-graph-analyzer';
-import {
-  buildDependencyGraph,
-  buildReverseDependencyGraphFromDependencyGraph,
-} from './dependency-graph-builder';
 import buildFineGrainedDependencyChain from './fine-grained-dependency-chain-builder';
 import processGitDiffString, { ChangedFile } from './git-diff-processor';
 import type { TssaResult } from './tssa-result';
@@ -16,13 +10,10 @@ import TypeScriptProjects from './typescript-projects';
 /** Run all available analysis on the change. */
 const getTSSAResult = (projectPaths: readonly string[], diffString: string): TssaResult => {
   const changedTSFiles: ChangedFile[] = [];
-  const changedCssPaths: string[] = [];
 
   processGitDiffString(diffString).forEach((changedFile) => {
     const normalizedPath = normalize(changedFile.sourceFilePath);
-    if (normalizedPath.endsWith('.css') || normalizedPath.endsWith('.scss')) {
-      changedCssPaths.push(normalizedPath);
-    } else if (
+    if (
       normalizedPath.endsWith('.ts') ||
       normalizedPath.endsWith('.tsx') ||
       normalizedPath.endsWith('.js') ||
@@ -41,19 +32,6 @@ const getTSSAResult = (projectPaths: readonly string[], diffString: string): Tss
     }
   });
 
-  const projectAndChangedTSPaths = partitionProjectChangedModulePaths(
-    projectPaths,
-    changedTSFiles,
-    (it) => it.sourceFilePath
-  );
-  const projectAndChangedCssPaths = partitionProjectChangedModulePaths(
-    projectPaths,
-    changedCssPaths,
-    (it) => it
-  );
-  const relevantProjectPaths = [...projectAndChangedTSPaths, ...projectAndChangedCssPaths].map(
-    (it) => it.projectPath
-  );
   const typescriptProjects = new TypeScriptProjects(projectPaths);
 
   const changedTSFileReferenceAnalysisResult = changedTSFiles.map((changedFile) => {
@@ -81,29 +59,7 @@ const getTSSAResult = (projectPaths: readonly string[], diffString: string): Tss
     };
   });
 
-  let allCssDependencyChain: Record<string, string[]> = {};
-
-  const forwardDependencyGraph = buildDependencyGraph(typescriptProjects, relevantProjectPaths);
-  const reverseDependencyGraph = buildReverseDependencyGraphFromDependencyGraph(
-    forwardDependencyGraph
-  );
-  changedCssPaths.forEach((changedCssPath) => {
-    const forwardDependencyChain = getTopologicallyOrderedTransitiveDependencyChainFromTSModules(
-      forwardDependencyGraph,
-      [changedCssPath]
-    );
-    const reverseDependencyChain = getTopologicallyOrderedTransitiveDependencyChainFromTSModules(
-      reverseDependencyGraph,
-      [changedCssPath]
-    );
-
-    allCssDependencyChain = { ...forwardDependencyChain, ...reverseDependencyChain };
-  });
-
-  return {
-    typescriptAnalysisResult: changedTSFileReferenceAnalysisResult,
-    cssAnalysisResult: allCssDependencyChain,
-  };
+  return changedTSFileReferenceAnalysisResult;
 };
 
 export default getTSSAResult;
--- src/tssa-result.ts
+++ src/tssa-result.ts
@@ -8,21 +8,7 @@ type ChangedFileReport = {
   readonly affectedFunctionChain: readonly AffectedFileWithSymbols[];
 };
 
-export type TssaResult = {
-  readonly typescriptAnalysisResult: readonly ChangedFileReport[];
-  /** A list of css affect chain. TODO: separate the result per file. */
-  readonly cssAnalysisResult: Record<string, string[]>;
-};
-
-const dependencyListToStringByFile = (list: Record<string, string[]>): string => {
-  let comment = '';
-  Object.keys(list).forEach((k) => {
-    comment += `Your changes to \`${k}\` affect: \n ${list[k]
-      .map((it) => `> \`${it}\``)
-      .join('\n')}`;
-  });
-  return comment;
-};
+export type TssaResult = readonly ChangedFileReport[];
 
 const dependencyListToString = (list: readonly string[]): string =>
   list.map((it) => `> \`${it}\``).join('\n');
@@ -30,10 +16,7 @@ const dependencyListToString = (list: readonly string[]): string =>
 const affectedFileWithSymbolsToString = ({ filename, symbols }: AffectedFileWithSymbols): string =>
   `${filename} > ${symbols.join(', ')}`;
 
-export const tssaResultToString = ({
-  typescriptAnalysisResult,
-  cssAnalysisResult,
-}: TssaResult): string => {
+export const tssaResultToString = (typescriptAnalysisResult: TssaResult): string => {
   const tsDependencyAnalysisResultStrings = typescriptAnalysisResult.map(
     ({ changedFilePath, affectedFunctionChain }) => {
       if (affectedFunctionChain.length === 0) return null;
@@ -43,17 +26,9 @@ ${dependencyListToString(affectedFunctionChain.map(affectedFileWithSymbolsToStri
     }
   );
 
-  const cssAnalysisResultString =
-    Object.keys(cssAnalysisResult).length === 0
-      ? null
-      : `Modules that your changes in css code may affect:
-
-${dependencyListToStringByFile(cssAnalysisResult)}`;
-
-  const analysisResultStrings = [
-    ...tsDependencyAnalysisResultStrings,
-    cssAnalysisResultString,
-  ].filter((it): it is string => it != null);
+  const analysisResultStrings = tsDependencyAnalysisResultStrings.filter(
+    (it): it is string => it != null
+  );
   return analysisResultStrings.length === 0
     ? 'No notable changes.'
     : analysisResultStrings.join('\n\n');
