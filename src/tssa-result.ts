8c0b44282b3277a5f389405cf201283777236752
Sam Zhou <sam@developersam.com>
2020-11-09 16:32:37 -0500

    [printing] Hide large change list by default (#112)

--- src/tssa-result.ts
+++ src/tssa-result.ts
@@ -15,7 +15,20 @@ export type TssaResult = readonly FileChangeImpact[];
 const dependencyListToString = (list: readonly string[]): string =>
   list.map((it) => `> \`${it}\``).join('\n');
 
+const hiddenByDefaultStringWrapper = (content: string): string => `<details>
+<summary>There are too many lines in change impact analysis report. It is hidden by default.</summary>
+${content}
+</details>`;
+
 export const tssaResultToString = (typescriptAnalysisResult: TssaResult): string => {
+  const impactListTotalLength = typescriptAnalysisResult.reduce(
+    (accumulator, impact) =>
+      impact.type === 'DIRECTLY_AFFECT_ONLY'
+        ? accumulator + impact.affected.length
+        : accumulator + impact.indirectlyAffected.length + 1,
+    0
+  );
+
   const tsDependencyAnalysisResultStrings = typescriptAnalysisResult.map((it) => {
     if (it.type === 'DIRECTLY_AFFECT_ONLY') {
       return `Your changes in \`${it.filename}\` may directly affect:
@@ -30,7 +43,9 @@ It will also affect the following files. Make sure you fully test those changes!
 ${dependencyListToString(it.indirectlyAffected)}`;
   });
 
-  return tsDependencyAnalysisResultStrings.length === 0
-    ? 'No notable changes.'
-    : tsDependencyAnalysisResultStrings.join('\n\n');
+  const result =
+    tsDependencyAnalysisResultStrings.length === 0
+      ? 'No notable changes.'
+      : tsDependencyAnalysisResultStrings.join('\n\n');
+  return impactListTotalLength > 25 ? hiddenByDefaultStringWrapper(result) : result;
 };
