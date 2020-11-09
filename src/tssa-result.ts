5f0c08b4d70f8896a3739da905d4000595987fac
Sam Zhou <sam@developersam.com>
2020-11-09 16:23:34 -0500

    [analysis] Decouple change impact classification with printing (#111)

--- src/tssa-result.ts
+++ src/tssa-result.ts
@@ -1,41 +1,36 @@
-import type { FileReferenceTree } from './fine-grained-dependency-tree-builder';
-
-export type TssaResult = readonly FileReferenceTree[];
+export type FileChangeImpact =
+  | {
+      readonly type: 'DIRECTLY_AFFECT_ONLY';
+      readonly filename: string;
+      readonly affected: readonly string[];
+    }
+  | {
+      readonly type: 'BOTH_DIRECT_AND_MULTIPLE_INDIRECT';
+      readonly filename: string;
+      readonly directlyAffected: string;
+      readonly indirectlyAffected: readonly string[];
+    };
+export type TssaResult = readonly FileChangeImpact[];
 
 const dependencyListToString = (list: readonly string[]): string =>
   list.map((it) => `> \`${it}\``).join('\n');
 
-const getDivergingPoint = (tree: FileReferenceTree): readonly string[] | null => {
-  if (tree.children.length === 0) return null;
-  if (tree.children.length >= 2) return tree.children.map((it) => it.value);
-  return getDivergingPoint(tree.children[0]);
-};
-
 export const tssaResultToString = (typescriptAnalysisResult: TssaResult): string => {
-  const tsDependencyAnalysisResultStrings = typescriptAnalysisResult.map((tree) => {
-    if (tree.children.length === 0) return null;
-    const divergingPoint = getDivergingPoint(tree);
-    if (divergingPoint == null) {
-      return `Your changes in \`${tree.value}\` will directly affect: \`${tree.children[0].value}\``;
-    }
-    const directChildren = tree.children.map((it) => it.value);
-    if (directChildren.length > 1) {
-      return `Your changes in \`${tree.value}\` may directly affect:
+  const tsDependencyAnalysisResultStrings = typescriptAnalysisResult.map((it) => {
+    if (it.type === 'DIRECTLY_AFFECT_ONLY') {
+      return `Your changes in \`${it.filename}\` may directly affect:
 
-${dependencyListToString(directChildren)}`;
+${dependencyListToString(it.affected)}`;
     }
 
-    return `Your changes in \`${tree.value}\` may directly affect: \`${directChildren[0]}\`.
+    return `Your changes in \`${it.filename}\` may directly affect: \`${it.directlyAffected}\`.
 
 It will also affect the following files. Make sure you fully test those changes!
 
-${dependencyListToString(divergingPoint)}`;
+${dependencyListToString(it.indirectlyAffected)}`;
   });
 
-  const analysisResultStrings = tsDependencyAnalysisResultStrings.filter(
-    (it): it is string => it != null
-  );
-  return analysisResultStrings.length === 0
+  return tsDependencyAnalysisResultStrings.length === 0
     ? 'No notable changes.'
-    : analysisResultStrings.join('\n\n');
+    : tsDependencyAnalysisResultStrings.join('\n\n');
 };
